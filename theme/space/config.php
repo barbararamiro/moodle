11c2bb29b0a09aac055921c5e8a66ec701111386
Barbara Ramiro <barbara@moodle.com>
2013-07-31 23:50:53 +0800

    MDLSITE-2332 theme: experimental theme derived from Clean

--- theme/space/config.php
+++ theme/space/config.php
@@ -38,7 +38,7 @@ $THEME->name = 'space';
 //
 $THEME->doctype = 'html5';
 $THEME->parents = array('bootstrapbase');
-$THEME->sheets = array('custom');
+$THEME->sheets = array('space', 'custom');
 $THEME->supportscssoptimisation = false;
 $THEME->yuicssmodules = array();
 
@@ -53,6 +53,12 @@ $THEME->plugins_exclude_sheets = array(
     ),
 );
 
+$THEME->parents_exclude_sheets = array(
+    'bootstrapbase' => array(
+            'moodle',
+        ),
+);
+
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
 $THEME->csspostprocess = 'theme_space_process_css';
 
@@ -61,124 +67,12 @@ $THEME->blockrtlmanipulations = array(
     'side-post' => 'side-pre'
 );
 
-// $THEME->layouts = array(
-// // The site home page.
-//     'frontpage' => array(
-//         'file' => 'columns2.php',
-//         'regions' => array('side-pre'),
-//         'defaultregion' => 'side-pre',
-//         'options' => array('nonavbar'=>true),
-//     ),
-//  );
-
 $THEME->layouts = array(
-    // Most backwards compatible layout without the blocks - this is the layout used by default.
-    'base' => array(
-        'file' => 'columns1.php',
-        'regions' => array(),
-    ),
-    // Standard layout with blocks, this is recommended for most pages with general information.
-    'standard' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-    ),
-    // Main course page.
-    'course' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-        'options' => array('langmenu'=>true),
-    ),
-    'coursecategory' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-    ),
-    // part of course, typical for modules - default page layout if $cm specified in require_login()
-    'incourse' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-    ),
-    // The site home page.
+	// The site home page.
     'frontpage' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-        'options' => array('nonavbar'=>true),
-    ),
-    // Server administration scripts.
-    'admin' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-    ),
-    // My dashboard page.
-    'mydashboard' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-        'options' => array('langmenu'=>true),
-    ),
-    // My public page.
-    'mypublic' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-    ),
-    'login' => array(
-        'file' => 'columns1.php',
-        'regions' => array(),
-        'options' => array('langmenu'=>true),
-    ),
-
-    // Pages that appear in pop-up windows - no navigation, no blocks, no header.
-    'popup' => array(
-        'file' => 'columns1.php',
-        'regions' => array(),
-        'options' => array('nofooter'=>true, 'nonavbar'=>true),
-    ),
-    // No blocks and minimal footer - used for legacy frame layouts only!
-    'frametop' => array(
-        'file' => 'columns1.php',
-        'regions' => array(),
-        'options' => array('nofooter'=>true, 'nocoursefooter'=>true),
-    ),
-    // Embeded pages, like iframe/object embeded in moodleform - it needs as much space as possible
-    'embedded' => array(
-        'file' => 'embedded.php',
-        'regions' => array()
-    ),
-    // Used during upgrade and install, and for the 'This site is undergoing maintenance' message.
-    // This must not have any blocks, and it is good idea if it does not have links to
-    // other places - for example there should not be a home link in the footer...
-    'maintenance' => array(
-        'file' => 'columns1.php',
-        'regions' => array(),
-        'options' => array('nofooter'=>true, 'nonavbar'=>true, 'nocoursefooter'=>true, 'nocourseheader'=>true),
-    ),
-    // Should display the content and basic headers only.
-    'print' => array(
-        'file' => 'columns1.php',
-        'regions' => array(),
-        'options' => array('nofooter'=>true, 'nonavbar'=>false),
-    ),
-    // The pagelayout used when a redirection is occuring.
-    'redirect' => array(
-        'file' => 'embedded.php',
-        'regions' => array(),
-    ),
-    // The pagelayout used for reports.
-    'report' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-    ),
-    // The pagelayout used for safebrowser and securewindow.
-    'secure' => array(
-        'file' => 'secure.php',
+        'file' => 'columns3.php',
         'regions' => array('side-pre', 'side-post'),
-        'defaultregion' => 'side-pre'
+        'defaultregion' => 'side-pre',
+        // 'options' => array('nonavbar'=>true),
     ),
-);
+ );
